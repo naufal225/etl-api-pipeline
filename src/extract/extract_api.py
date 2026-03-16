9850d72839b0a37fdfda9282af773d81923a8d2b
naufal225 <naufalmarufashrori225@gmail.com>
2026-03-17 00:38:40 +0700

    memperbaiki export_api.py

--- src/extract/extract_api.py
+++ src/extract/extract_api.py
@@ -1,8 +1,8 @@
 import requests
-import pandas as pd
 from pathlib import Path
 import sys
 import os
+import json
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -12,39 +12,46 @@ import time
 BASE_DIR = Path(__file__).resolve().parents[2]
 
 RAW_DIR = BASE_DIR / "data" / "raw"
-PROCESSDED_DIR = BASE_DIR / "data" / "processed"
+PROCESSED_DIR = BASE_DIR / "data" / "processed"
 
 LOG_PATH = BASE_DIR / "logs" / "etl-run.log"
 
 RAW_DIR.mkdir(parents=True, exist_ok=True)
-PROCESSDED_DIR.mkdir(parents=True, exist_ok=True)
+PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
+
+BASE_URL = "https://jsonplaceholder.typicode.com"
+ENDPOINTS = {
+    "users" : "/users",
+    "posts" : "/posts",
+    "comments" : "/comments"
+}
 
 logger = build_logger(LOG_PATH)
 
-def fetch_json(url: str):
+def fetch_json(url: str, endpoint_name: str):
     logger.info("Fetching into %s", url)
     start = time.time()
     r = requests.get(url=url, timeout=10)
     r.raise_for_status()
     end = time.time() - start
-    logger.info("Success fecth %s for %ds", url, end)
-    return r
+    
+    data = r.json()
+    
+    rows_count = len(data) #sekarang ok, tapi jadi masalah kalau struktur nya ada status, code, message, baru data
+    
+    logger.info(f"Fetched {endpoint_name} endpoint | rows = {rows_count} | duration = {end:.2f}s")
+    return data
 
 def save_json(data, path: Path):
-    df = pd.DataFrame(data)
-    df.to_json(path, orient="records")
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2)
     
 def extract_all():
-    users = fetch_json("https://jsonplaceholder.typicode.com/users")
-    posts = fetch_json("https://jsonplaceholder.typicode.com/posts")
-    comments = fetch_json("https://jsonplaceholder.typicode.com/comments")
-    
-    save_json(users, BASE_DIR / "users.json")
-    logger.info("Success, saved users to %s", BASE_DIR / "users.json")
-    save_json(posts, BASE_DIR / "posts.json")
-    logger.info("Success, saved posts to %s", BASE_DIR / "posts.json")
-    save_json(comments, BASE_DIR / "comments.json")
-    logger.info("Success, saved comments to %s", BASE_DIR / "comments.json")
+    for k, v in ENDPOINTS.items():
+        r = fetch_json(BASE_URL + v, k)
+        file_name = k + ".json"
+        save_json(r, RAW_DIR / file_name)
+        logger.info("Success, saved %s to %s", k, RAW_DIR / file_name)
     
 if __name__ == "__main__":
     extract_all()
